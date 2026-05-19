fd2a8880701a3c695d55cdf11165325108512930
etienne <laurentlionel20@hotmail.com>
2026-05-19 09:31:40 +0200

    [Add] base solid js project - Add daisyui

---
+++ hex-front/src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div class="h-screen bg-green-500 flex justify-center items-center ">
+      <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    </div>
+  );
+};
+
+export default App;
